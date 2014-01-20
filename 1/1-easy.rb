992679b214a1f716e0ebce515009fba55a8acc9a
notaku <keisaku@autistici.org>
2014-01-20 14:10:53 +1100

    first commit

---
+++ 1/1-easy.rb
@@ -0,0 +1,12 @@
+# https://pay.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+
+print "What is your name?\n> "
+name = gets.chomp
+
+print "How old are you?\n> "
+age = gets.chomp
+
+print "What is your Reddit username?\n> "
+username = gets.chomp
+
+puts "Your name is #{name}, you are #{age} years old, and your username is #{username}."
